58c0d49b24ff16b881febb6a188c186cd6fce96b
mosenos-loisenicole <loisenicolemosenos@gmail.com>
2024-10-09 22:51:21 +0800

    Update alejandro_aleckmcklaiyre.py

--- alejandro_aleckmcklaiyre.py
+++ alejandro_aleckmcklaiyre.py
@@ -3,6 +3,7 @@ print("Birthday: September 03, 2004")
 print("Gender: Female")
 print("Email: aleck.alejandro04@gmail.com")
 
+#welcome to the team
 i edit this file - jasmine
 hatdog -jasmine
 Edit - Mik
