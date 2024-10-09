3ebec6740d0fd2d63553034cd7f4a160ad6774c8
ashley hermione gomez <hermionegomez49@gmail.com>
2024-10-09 10:32:55 +0800

    update a name

--- mosenos_loisenicole.py
+++ mosenos_loisenicole.py
@@ -1,4 +1,5 @@
-print("Name: Loise Nicole S. Mosenos)
+print("Name: Loise Nicole S. Moseños")
 print("Birthday: March 19, 2005")
 print("Gender: Female")
 print("Email: loisenicolemosenos@gmail.com")
+print ("Welcome to the team - Ashley")
