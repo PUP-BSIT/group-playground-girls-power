33450552800cd71aba240cccca8ff62daec3ab89
Mikka Kette <esparagozamikkakette@gmail.com>
2024-12-11 02:41:22 +0800

    edit

--- pull_request/main.py
+++ pull_request/main.py
@@ -2,7 +2,7 @@ from package import alejandro, deleon, mosenos, esparagoza
 
 def main_menu():
     while True:
-        print("\n---Main Menu ---")
+        print("---Main Menu---")
         print("1. Aleck Mcklaiyre Alejandro")
         print("2. Jasmine Robelle De leon")
         print("3. Mikka Kette Esparagoza")
@@ -13,31 +13,28 @@ def main_menu():
         choice = input("Choose an option: ")
         
         if choice == '1':
-
             print("-----------------------------------")  
             alejandro.aleck_info()
 
         elif choice == '2':
-            print("\n---  ---")
+            print("---  ---")
             deleon.jasmine_info()
             
         elif choice == '3':
-            print("-------------------------------")
+            print("-----------------------------------")
             esparagoza.esparagoza_menu()
 
         elif choice == '4':
-
-            print("------------------------")
+            print("-----------------------------------")
 
         elif choice == '5':
-
-            print("---------------------------------------")   
+            print("-----------------------------------")   
             mosenos.loise_info() 
-
             
         elif choice == '6':
             print("\nExiting program.\n")
             break
+
         else:
             print("\nInvalid option. Please try again.\n")
 
