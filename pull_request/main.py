ac0913b60e1a959471179693d516917ad7a62e76
alejandro-aleck <aleck.alejandro04@gmail.com>
2024-12-11 01:18:57 +0800

    Main menu

---
+++ pull_request/main.py
@@ -0,0 +1,44 @@
+from package import alejandro
+
+def main_menu():
+    while True:
+        print("\n---Main Menu ---")
+        print("1. Aleck Mcklaiyre Alejandro")
+        print("2. Jasmine Robelle De leon")
+        print("3. Mikka Kette Esparagoza")
+        print("4. Ashley Hermoine Gomez")
+        print("5. Loise Nicole Mosenos")
+        print("6. Exit")
+        
+        choice = input("Choose an option: ")
+        
+        if choice == '1':
+
+            print("-----------------------------------")  
+            alejandro.aleck_info()
+
+        elif choice == '2':
+            print("\n---  ---")
+            
+        elif choice == '3':
+            print("-------------------------------")
+
+
+        elif choice == '4':
+
+            print("------------------------")
+
+        elif choice == '5':
+
+            print("---------------------------------------")     
+
+            
+        elif choice == '6':
+            print("\nExiting program.\n")
+            break
+        else:
+            print("\nInvalid option. Please try again.\n")
+
+# Run the main menu
+if __name__ == "__main__":
+    main_menu()
